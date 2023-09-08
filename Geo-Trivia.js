f9655945b11d96585e3df2d40ec9c8ebf09d611c
Hannan450 <140449208+Hannan450@users.noreply.github.com>
2023-09-08 11:35:28 -0400

    first draft trivia site

---
+++ Geo-Trivia.js
@@ -0,0 +1,14 @@
+
+var play_button = document.getElementById("play-btn");
+var content_div = document.getElementById("game-container");
+
+play_button.addEventListener("click", function() {
+    var currentStyle = content_div.style.display;
+    content_div.style.display = "flex";
+    
+});
+
+var next_button = document.getElementById("next-btn");
+
+
+var submit_button = document.getElementById("submit-btn");
